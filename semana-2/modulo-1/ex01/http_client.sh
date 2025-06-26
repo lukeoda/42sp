81d7f2d53e9170ed9e9a99661723efc654d116ee
LUCAS KENJI ODA <lkenji-o@c2r2p2.42sp.org.br>
2025-06-26 17:17:47 -0300

    push semana 1 e 2

--- semana-2/modulo-1/ex01/http_client.sh
+++ semana-2/modulo-1/ex01/http_client.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 chmod +x ./http_client.sh
-curl -i -H "Authorization: token " https://api.github.com/users/torvalds -o jsonfile.json 
+curl -i -H "Authorization: " https://api.github.com/users/torvalds -o jsonfile.json 
 
 
